63e33d41198f8a86bef3c6cfb37c872bb483d8b3
Antony Male <antony.male@gmail.com>
2011-06-06 15:13:33 +0100

    Basic handing of javascript files. Can't handle minification, but supports javascript groups, enabling/disabling groups, rendering groups (both inline and not), asset paths, folders, etc....

---
+++ classes/casset.php
@@ -0,0 +1,175 @@
+<?php
+
+class Casset {
+
+	protected static $asset_paths = array();
+
+	protected static $asset_url = '/';
+
+	protected static $folders = array(
+		'css' => 'css/',
+		'js' => 'js/',
+		'img' => 'img/',
+	);
+
+	protected static $groups = array(
+		'css' => array('global' => array('files' => array(), 'enabled' => true)),
+		'js' => array('global' => array('files' => array(), 'enabled' => true)),
+	);
+
+	protected static $min = false;
+
+	public static $initialized = false;
+
+	public static function _init()
+	{
+		// Prevent multiple initializations
+		if (static::$initialized)
+		{
+			return;
+		}
+
+		Config::load('casset', true);
+
+		$paths = Config::get('casset.paths', array('assets/'));
+
+		foreach($paths as $path)
+		{
+			static::add_path($path);
+		}
+
+		static::$asset_url = Config::get('casset.url', Config::get('base_url'));
+
+		static::$folders = array(
+			'css' => Config::get('casset.css_dir', static::$folders['css']),
+			'js' => Config::get('casset.js_dir', static::$folders['js']),
+			'img' => Config::get('casset.img_dir', static::$folders['img']),
+		);
+
+		$group_sets = Config::get('casset.groups', array());
+
+		foreach ($group_sets as $group_type => $groups)
+		{
+			foreach ($groups as $group_name => $group)
+			{
+				static::add_group($group_type, $group_name, $group);
+			}
+		}
+
+		static::$min = Config::get('casset.min', static::$min);
+
+		static::$initialized = true;
+	}
+
+	public static function add_path($path)
+	{
+		array_unshift(static::$asset_paths, str_replace('../', '', $path));
+	}
+
+	public static function remove_path($path)
+	{
+		if (($key = array_search(str_replace('../', '', $path), static::$_asset_paths)) !== false)
+		{
+			unset(static::$_asset_paths[$key]);
+		}
+	}
+
+	public static function add_group($group_type, $group_name, $group)
+	{
+		static::$groups[$group_type][$group_name] = $group;
+	}
+
+	public function find_file($file, $asset_type)
+	{
+		if (strpos($file, '//') === false)
+		{
+			$folder = static::$folders[$asset_type];
+			$file = ltrim($file, '/');
+
+			foreach (static::$asset_paths as $path)
+			{
+				if (is_file($path.$folder.$file))
+				{
+					return static::$asset_url.$path.$folder.$file;
+				}
+			}
+			throw new Fuel_Exception('Coult not find asset: '.$file);
+		}
+		else
+		{
+			return $file;
+		}
+	}
+
+	public static function enable_js($group)
+	{
+		if (!array_key_exists($group, static::$groups['js']))
+				return;
+		static::$groups['js'][$group]['enabled'] = true;
+	}
+
+	public static function disable_js($group)
+	{
+		if (!array_key_exists($group, static::$groups['js']))
+				return;
+		static::$groups['js'][$group]['enabled'] = false;
+	}
+
+	public static function js($script, $script_min = false)
+	{
+		array_push(static::$groups['js']['global']['files'], array($script, $script_min));
+	}
+
+	public function render_js($group = false, $inline = false, $min = false)
+	{
+		// Very simple minimisation for now
+		$min = $min || static::$min;
+		if ($group == false)
+		{
+			$group_names = array_keys(static::$groups['js']);
+		}
+		else
+		{
+			$group_names = array($group);
+			// If they're specificially asked to render a group, enable it
+			static::$groups['js'][$group]['enabled'] = true;
+		}
+
+		$files = array();
+
+		foreach ($group_names as $group_name)
+		{
+			if (static::$groups['js'][$group_name]['enabled'] == false)
+				continue;
+
+			foreach (static::$groups['js'][$group_name]['files'] as $file_set)
+			{
+				array_push($files, static::find_file(($min) ? $file_set[1] : $file_set[0], 'js'));
+			}
+		}
+
+		$ret = '';
+
+		foreach ($files as $file)
+		{
+			if (!$min)
+			{
+				if ($inline) {
+					$ret .= html_tag('script', array('type' => 'text/javascript'), PHP_EOL.file_get_contents($file).PHP_EOL).PHP_EOL;
+				}
+				else
+				{
+					$ret .= html_tag('script', array(
+						'type' => 'text/javascript',
+						'src' => $file,
+					)).PHP_EOL;
+				}
+			}
+		}
+		return $ret;
+	}
+
+
+}
+
+/* End of file casset.php */
