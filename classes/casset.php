e91fc3eeaf0cb3588193d986c6cab1b5eb497c71
Antony Male <antony.male@gmail.com>
2011-06-06 17:13:34 +0100

    Minifying javascript now works. However, CSS rendering will be completely borked for the time being.

--- classes/casset.php
+++ classes/casset.php
@@ -9,15 +9,16 @@ class Casset {
 	protected static $folders = array(
 		'css' => 'css/',
 		'js' => 'js/',
-		'img' => 'img/',
 	);
+
+	protected static $cache_path = 'assets/cache/';
 
 	protected static $groups = array(
 		'css' => array('global' => array('files' => array(), 'enabled' => true, 'const' => false)),
 		'js' => array('global' => array('files' => array(), 'enabled' => true, 'const' => false)),
 	);
 
-	protected static $min = false;
+	protected static $min = true;
 
 	public static $initialized = false;
 
@@ -43,8 +44,9 @@ class Casset {
 		static::$folders = array(
 			'css' => Config::get('casset.css_dir', static::$folders['css']),
 			'js' => Config::get('casset.js_dir', static::$folders['js']),
-			'img' => Config::get('casset.img_dir', static::$folders['img']),
 		);
+
+		static::$cache_path = Config::get('casset.cache_path', static::$cache_path);
 
 		$group_sets = Config::get('casset.groups', array());
 
@@ -94,7 +96,7 @@ class Casset {
 			{
 				if (is_file($path.$folder.$file))
 				{
-					return static::$asset_url.$path.$folder.$file;
+					return $path.$folder.$file;
 				}
 			}
 			throw new Fuel_Exception('Coult not find asset: '.$file);
@@ -164,22 +166,67 @@ class Casset {
 		if ($min === null)
 			$min = static::$min;
 
-		$files = static::files_to_render('js', $group, $min);
+		$file_groups = static::files_to_render('js', $group, $min);
 
 		$ret = '';
 
-		foreach ($files as $file)
+		foreach ($file_groups as $group_name => $file_group)
 		{
-			if (!$min)
+			if ($min)
 			{
+				// Generate combined filename
+				// The 'global' group is non-const, and needs an md5 as the filename
+				// Other groups are const, and filename can be derived from the group name
+				if ($group_name == 'global')
+					$filename = md5(implode('', array_map(function($a) {
+						return $a['file'];
+					}, $file_group)));
+				else
+					$filename = 'group_'.$group_name.'.js';
+				// Get the last modified time of all of the component files
+				$last_mod = 0;
+				foreach ($file_group as $file)
+				{
+					$mod = filemtime(DOCROOT.$file['file']);
+					if ($mod > $last_mod)
+						$last_mod = $mod;
+				}
+				$needs_update = (!file_exists(DOCROOT.$filename) || filemtime(DOCROOT.$filename) < $last_mod);
+
+				if ($needs_update)
+				{
+					$content = '';
+					foreach ($file_group as $file)
+					{
+						if ($file['minified'])
+							$content .= file_get_contents($file['file']).PHP_EOL;
+						else
+							$content = Casset_JSMin::minify(file_get_contents($file['file'])).PHP_EOL;
+					}
+					file_put_contents(DOCROOT.static::$cache_path.'/'.$filename, $content);
+				}
 				if ($inline)
-					$ret .= html_tag('script', array('type' => 'text/javascript'), PHP_EOL.file_get_contents($file).PHP_EOL).PHP_EOL;
+					$ret .= html_tag('script', array('type' => 'text/javascript'), PHP_EOL.file_get_contents(DOCROOT.static::$cache_path.'/'.$filename).PHP_EOL).PHP_EOL;
 				else
 					$ret .= html_tag('script', array(
 						'type' => 'text/javascript',
-						'src' => $file,
+						'src' => static::$asset_url.static::$cache_path.$filename,
 					)).PHP_EOL;
 			}
+			else
+			{
+				foreach ($file_group as $file)
+				{
+					if ($inline)
+						$ret .= html_tag('script', array('type' => 'text/javascript'), PHP_EOL.file_get_contents($file['file']).PHP_EOL).PHP_EOL;
+					else
+						$ret .= html_tag('script', array(
+							'type' => 'text/javascript',
+							'src' => $file['file'],
+						)).PHP_EOL;
+				}
+			}
+
 		}
 		return $ret;
 	}
@@ -217,17 +264,41 @@ class Casset {
 		else
 			$group_names = array($group);
 
+		// All const groups get their own key in this array
+		// All non-const groups get munged into the 'global' group
 		$files = array();
+
+		$minified = false;
 
 		foreach ($group_names as $group_name)
 		{
 			if (static::$groups[$type][$group_name]['enabled'] == false)
 				continue;
 
+			$file_group_name = static::$groups[$type][$group_name]['const'] ? $group_name : 'global';
+			if (!array_key_exists($file_group_name, $files))
+				$files[$file_group_name] = array();
+
 			foreach (static::$groups[$type][$group_name]['files'] as $file_set)
 			{
-				array_push($files, static::find_file(($min) ? $file_set[1] : $file_set[0], $type));
+				if ($min)
+				{
+					$file = static::find_file(($file_set[1]) ? $file_set[1] : $file_set[0], $type);
+					$minified = ($file_set[1] != false);
+				}
+				else
+				{
+					$file = static::find_file($file_set[0], $type);
+				}
+				array_push($files[$file_group_name], array(
+					'file' => $file,
+					'minified' => $minified,
+				));
 			}
+			// If minifying, sort by filename
+			uasort($files[$file_group_name], function($a, $b) {
+				return ($a['file'] > $b['file']) ? 1 : -1;
+			});
 		}
 		return $files;
 	}
