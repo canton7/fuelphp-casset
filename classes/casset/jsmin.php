2031686837f30d8f48f828096d9a73c9833e7568
Antony Male <antony.male@gmail.com>
2011-06-09 13:55:16 +0100

    Somehow namespacing was incorrect inside of jsmin. Fixed!

--- classes/casset/jsmin.php
+++ classes/casset/jsmin.php
@@ -320,8 +320,8 @@ class Casset_JSMin {
     }
 }
 
-class JSMin_UnterminatedStringException extends Exception {}
-class JSMin_UnterminatedCommentException extends Exception {}
-class JSMin_UnterminatedRegExpException extends Exception {}
+class JSMin_UnterminatedStringException extends \Exception {}
+class JSMin_UnterminatedCommentException extends \Exception {}
+class JSMin_UnterminatedRegExpException extends \Exception {}
 
 /* End of file casset/jsmin.php */
