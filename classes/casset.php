7d8fd49fd36c24247bcd7974bd62b218c7c97835
Antony Male <antony.male@gmail.com>
2011-09-25 19:27:15 +0100

    Bump version to v1.11

--- classes/casset.php
+++ classes/casset.php
@@ -4,7 +4,7 @@
  * Casset: Convenient asset library for FuelPHP.
  *
  * @package    Casset
- * @version    v1.10
+ * @version    v1.11
  * @author     Antony Male
  * @license    MIT License
  * @copyright  2011 Antony Male
