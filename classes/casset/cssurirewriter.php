4eb06249b06e1caef4ba321d16de2ad2a2d6b572
Antony Male <antony.male@gmail.com>
2012-02-04 01:26:33 +0000

    Bump version to v1.12

--- classes/casset/cssurirewriter.php
+++ classes/casset/cssurirewriter.php
@@ -10,7 +10,7 @@
  * This library is used as part of Casset.
  *
  * @package    Casset
- * @version    v1.11
+ * @version    v1.12
  * @author     Antony Male
  * @license    MIT License
  * @link       http://github.com/canton7/fuelphp-casset
